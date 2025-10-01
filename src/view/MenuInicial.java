3978e213cb38f06a04a270873678bae5c6a03b4d
Gabriel <gabmineiro123@hotmail.com>
2025-10-01 10:23:23 -0400

    Menu criado: classe para menu inicial

---
+++ src/view/MenuInicial.java
@@ -0,0 +1,23 @@
+package view;
+
+import java.util.Scanner;
+
+public class MenuInicial {
+    private static int resposta;
+    private static Scanner in = new Scanner(System.in);
+
+    public static int inputMenu(){
+        printMenu();
+        resposta = Integer.valueOf(in.nextInt());
+        return resposta;
+    }
+
+    private static void printMenu(){
+        System.out.println("1 - Cadastrar um novo pet");
+        System.out.println("2 - Alterar os dados do pet cadastrado");
+        System.out.println("3 - Deletar um pet cadastrado");
+        System.out.println("4 - Listar todos os pets cadastrados");
+        System.out.println("5 - Listar pets por algum critério (idade, nome, raça)");
+        System.out.println("6 - Sair");
+    }
+}
